8de9585f835a8efa322e4863a107a1f302269b4a
Vlad-warlock <107559060+Vlad-warlock@users.noreply.github.com>
2022-07-10 13:23:59 +0300

    Create MaxNumber.java

---
+++ MaxNumber.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Максимальное число это - " + getMaxInteger(12, 21, 13));
+    }
+
+    static int getMaxInteger(int a, int b, int c) {
+        int[] arr = {a, b, c};
+        int max = Integer.MIN_VALUE;
+        for (int element: arr) {
+            if (element > max) {
+                max = element;
+            }
+        }
+        return max;
+    }
+}
